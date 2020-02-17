478113a5c4b6847e08f2f7263aa08c08d798fd51
Erninda <erninda.ristiani94@gmail.com>
2020-02-17 13:33:03 +0700

    Menyelesaikan Market Place Layout

---
+++ app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
@@ -0,0 +1,29 @@
+package id.putraprima.marketplacelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void clickKembaliLogin(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    public void next(View view) {
+        Intent intent = new Intent(RegisterActivity.this, ProfileActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
